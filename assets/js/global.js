bdb1e764ee71433fdb710fc9509a430251642524
Mudassir Ahmed <mudassir0ahmed1@gmail.com>
2024-04-01 08:14:01 +0500

    origin

--- assets/js/global.js
+++ assets/js/global.js
@@ -17,3 +17,53 @@ document.querySelectorAll(".toggleHeader").forEach((item) => {
     tarEl.classList.toggle("active");
   });
 });
+
+const header = document.querySelector("header");
+const sections = document.querySelectorAll("section");
+const navItems = document.querySelectorAll("nav span");
+
+const handleNavItem = (sectionId) => {
+  navItems.forEach((navItem) => {
+    if (navItem.innerHTML.toLowerCase() == sectionId?.toLowerCase()) {
+      navItem.classList.add("active");
+    } else {
+      navItem.classList.remove("active");
+    }
+  });
+};
+
+const handleSectionScroll = (id) => {
+  const targetElement = document.getElementById(id);
+  targetElement.scrollIntoView({ behavior: "smooth" });
+  handleNavItem(id);
+};
+
+function checkCurrentSection() {
+  const scrollPosition = window.scrollY;
+  const offset = 50; // Adjust this value as needed
+
+  for (let i = 0; i < sections.length; i++) {
+    const section = sections[i];
+    const rect = section.getBoundingClientRect();
+
+    // Check if the section is partially in view
+    if (rect.top - offset <= 0 && rect.bottom > offset) {
+      return section.id;
+    }
+  }
+
+  return null; // No section in view
+}
+
+const handleScroll = () => {
+  const sectionId = checkCurrentSection();
+  handleNavItem(sectionId);
+  console.log(sectionId == "home");
+  if (sectionId == "home") {
+    header.classList.remove("fixedHeader");
+  } else {
+    header.classList.add("fixedHeader");
+  }
+};
+
+window.addEventListener("scroll", handleScroll);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        large: "1930px"
+      }
+    }
   },
   plugins: []
 };
